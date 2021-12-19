3ef2e041869679ba0fb6fc6c0b85db030450ff6c
thenightsnail <88739578+thenightsnail@users.noreply.github.com>
2021-12-19 21:34:05 +0200

    Exercise 12.14 (#14)
    * Delete App.test.js
    
    * Add test stage
    
    * Add API_ROOT
    
    * Move Todo to separate component

--- todo-app/todo-frontend/src/Todos/TodoView.js
+++ todo-app/todo-frontend/src/Todos/TodoView.js
@@ -4,11 +4,13 @@ import axios from '../util/apiClient'
 import List from './List'
 import Form from './Form'
 
+const API_ROOT = process.env.REACT_APP_BACKEND_URL;
+
 const TodoView = () => {
   const [todos, setTodos] = useState([])
 
   const refreshTodos = async () => {
-    const { data } = await axios.get('/todos')
+    const { data } = await axios.get(`${API_ROOT}/todos`)
     setTodos(data)
   }
 
@@ -17,17 +19,17 @@ const TodoView = () => {
   }, [])
 
   const createTodo = async (todo) => {
-    const { data } = await axios.post('/todos', todo)
+    const { data } = await axios.post(`${API_ROOT}/todos`, todo)
     setTodos([...todos, data])
   }
 
   const deleteTodo = async (todo) => {
-    await axios.delete(`/todos/${todo._id}`)
+    await axios.delete(`${API_ROOT}/todos/${todo._id}`)
     refreshTodos()
   }
 
   const completeTodo = async (todo) => {
-    await axios.put(`/todos/${todo._id}`, {
+    await axios.put(`${API_ROOT}/todos/${todo._id}`, {
       text: todo.text,
       done: true
     })
